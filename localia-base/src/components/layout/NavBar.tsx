1168ecb58f7e17aaa22afe569a7317fb11edb463
LauraArroyo10 <la.arroyo.rivera@gmail.com>
2026-05-08 20:23:00 -0600

    nav bar

--- localia-base/src/components/layout/NavBar.tsx
+++ localia-base/src/components/layout/NavBar.tsx
@@ -1,33 +1,44 @@
 import { NavbarMenu } from "../mockData/data";
 
-export interface NavBarProps{
-
-}
-
+export interface NavBarProps {}
 
 function NavBar() {
   return (
-    <h1>
-      <nav className="container ">
-        <a className="navbar-brand" href="#">
-          <img
-            src="src/assets/brand/logo.svg"
-            width="150"
-            height="150"
-            className="d-inline-block align-top"
-            alt="localia logo"
-          />
+    <nav className="bg-bg border-b border-violet-700 flex items-center justify-between h-13  max-w-277 mx-auto">
+      {/* Logo */}
+      <div className="flex items-center gap-2">
+        <img
+          src="src/assets/brand/logo.svg"
+          alt="Localia"
+          className="w-30 h-30"
+        />
+      </div>
+
+
+      {/* Links */}
+      <div className="flex items-center gap-1">
+        <a
+          href="/"
+          className="text-sm text-violet-700 px-4 py-1.5  "
+        >
+          Home
         </a>
+        <a
+          href="/signin"
+          className="text-sm text-violet-700 px-4 py-1.5 "
+        >
+          Sign In
+        </a>
+        <a
+          href="/signup"
 
-        
-      </nav>
-
-    </h1>
+                className="text-sm font-medium text-neutral-0 bg-violet-700 px-4 py-1.5 rounded-3xl "
+        >
+          Sign Up
+        </a>
+      </div>
+    </nav>
   );
 }
 
 export default NavBar;
-
-
-
-
